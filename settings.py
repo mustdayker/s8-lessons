59d0c0617df362676d6f38b7c1e2095f2ba54fe5
Dmitry Kosarev <mustdayker@gmail.com>
2023-09-17 10:38:34 +0300

    up

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-USER_HOST = '158.160.49.238' # укажите свой хост
-TOPIC_NAME = 'student.topic.cohort<номер когорты>.<username>' 
+USER_HOST = '158.160.118.6' # укажите свой хост
+TOPIC_NAME = 'student.topic.cohort14.mustdayker' 
 # укажите название топика student.topic.cohort<номер когорты>.<username>
