9c73407b179f377e267ea3eeb60c6da4f0a30123
Dmitry Kosarev <mustdayker@gmail.com>
2023-09-24 20:43:04 +0300

    up

--- Тема 3. Настройка потока данных/6. Дедупликация/Задание 1/realization.py
+++ Тема 3. Настройка потока данных/6. Дедупликация/Задание 1/realization.py
@@ -1,30 +1,52 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as f
-from pyspark.sql.types import StructType, StructField, DoubleType, StringType
+from pyspark.sql.types import StructType, StructField, DoubleType, StringType, TimestampType
 
-# необходимая библиотека с идентификатором в maven
-# вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
-kafka_lib_id =
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
-
-# настройки security для кафки
-# вы можете использовать из с помощью метода .options(**kafka_security_options)
-kafka_security_options = {
-    'kafka.security.protocol': 'SASL_SSL',
-    'kafka.sasl.mechanism': 'SCRAM-SHA-512',
-    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";',
-}
+TOPIC_NAME = 'student.topic.cohort14.mustdayker'
 
 def spark_init() -> SparkSession:
-    pass
+    spark_jars_packages = ",".join(
+        [
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+        ]
+    )
+    return (SparkSession.builder
+            .master("local")
+            .appName('test stream')
+            .config("spark.jars.packages", spark_jars_packages)
+            .getOrCreate()
+            )
 
 
 def load_df(spark: SparkSession) -> DataFrame:
-    pass
+    return (spark.readStream
+            .format('kafka')
+            .option('kafka.bootstrap.servers', 'rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091')
+            .option("subscribe", TOPIC_NAME)        
+            .option('kafka.security.protocol', 'SASL_SSL')
+            .option('kafka.sasl.mechanism', 'SCRAM-SHA-512')
+            .option('kafka.sasl.jaas.config',
+                    'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";')
+            .load())
 
 
 def transform(df: DataFrame) -> DataFrame:
-    pass
+    schema = StructType([
+        StructField("client_id", StringType()),
+        StructField("timestamp", DoubleType()),
+        StructField("lat", DoubleType()),
+        StructField("lon", DoubleType())
+    ])
+
+    return (df
+            .withColumn('value', f.col('value').cast(StringType()))
+            .withColumn('event', f.from_json(f.col('value'), schema))
+            .selectExpr('event.*')
+            .withColumn('timestamp',
+                        f.from_unixtime(f.col('timestamp'), "yyyy-MM-dd' 'HH:mm:ss.SSS").cast(TimestampType()))
+            .withWatermark('timestamp', '10 minutes')
+            .dropDuplicates(subset=['client_id', 'timestamp'])
+            )
 
 
 spark = spark_init()
